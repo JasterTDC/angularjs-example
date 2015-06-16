523fc68ea4928fbc6e2554e8aaa83b215614ae92
Ismael Moral <jastertdc@icloud.com>
2015-06-16 12:27:12 +0200

    It works now

--- controllers/app.js
+++ controllers/app.js
@@ -1,22 +1,31 @@
 
 var app = angular.module ('flapperNews', []);
 
-app.controller ('MainCtrl', ['$scope', function ($scope){
-  $scope.test = 'Hello world !';
-  $scope.posts = [
-    { title: 'post 1', upvotes: 5 },
-    { title: 'post 2', upvotes: 8 },
-    { title: 'post 3', upvotes: 3 },
-    { title: 'post 4', upvotes: 2 }
-  ];
+app.factory ('posts', function (){
+  var o = {
+    posts : [
+      { title: 'post 1', upvotes: 5, link: 'https://www.google.com' },
+      { title: 'post 2', upvotes: 8, link: 'https://www.twitter.com' },
+      { title: 'post 3', upvotes: 3, link: 'https://www.facebook.com' },
+      { title: 'post 4', upvotes: 2, link: 'https://www.pordede.com' }
+    ]
+  };
+
+  return o;
+});
+
+app.controller ('MainCtrl', ['$scope', 'posts', function ($scope, posts){
+  console.log (posts.posts)
+  $scope.posts = posts.posts;
 
   $scope.addPost = function () {
     if (!$scope.title || $scope.title === '') { return ; }
     $scope.posts.push ({
-      title: $scope.title, upvotes : 0
+      title: $scope.title, upvotes : 0, link: $scope.link
     });
 
     $scope.title = '';
+    $scope.link = '';
   };
 
   $scope.incrementUpvotes = function (post){
